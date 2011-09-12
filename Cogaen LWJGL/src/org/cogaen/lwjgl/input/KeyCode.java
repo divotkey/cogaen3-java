e5e063a5fcda27c2a3a4e50fd610eae5dc0f0519
Roman Divotkey <roman@divotkey.net>
2011-09-12 10:32:09 +0200

    Added more key codes and UpdateableComponent.

--- Cogaen LWJGL/src/org/cogaen/lwjgl/input/KeyCode.java
+++ Cogaen LWJGL/src/org/cogaen/lwjgl/input/KeyCode.java
@@ -6,4 +6,7 @@ public class KeyCode {
 	public static final int KEY_RIGHT = 205;
 	public static final int KEY_UP = 200;
 	public static final int KEY_DOWN = 208;
+	public static final int KEY_MINUS = 12;
+	public static final int KEY_ADD = 78;
+	public static final int KEY_SUBTRACT = 74;
 }
