9fa4e037bfe07539e85d77dfbc7efd4117a9fb48
Roman Divotkey <roman@divotkey.net>
2011-09-15 13:39:34 +0200

    Resources are not identified by strings rather than CogaenIds.

--- Cogaen LWJGL/src/org/cogaen/lwjgl/scene/SpriteHandle.java
+++ Cogaen LWJGL/src/org/cogaen/lwjgl/scene/SpriteHandle.java
@@ -1,7 +1,6 @@
 package org.cogaen.lwjgl.scene;
 
 import org.cogaen.core.Core;
-import org.cogaen.name.CogaenId;
 import org.cogaen.resource.ResourceException;
 import org.cogaen.resource.ResourceHandle;
 import org.cogaen.resource.ResourceService;
@@ -12,14 +11,14 @@ public class SpriteHandle extends ResourceHandle {
 	private SpriteVisual sprite;
 	private double width;
 	private double height;
-	private CogaenId textureId;
+	private String textureName;
 	
-	public SpriteHandle(CogaenId textureId) {
-		this(textureId, 0, 0);
+	public SpriteHandle(String textureName) {
+		this(textureName, 0, 0);
 	}
 	
-	public SpriteHandle(CogaenId textureId, double width, double height) {
-		this.textureId = textureId;
+	public SpriteHandle(String textureName, double width, double height) {
+		this.textureName = textureName;
 		this.width = width;
 		this.height = height;
 	}
@@ -31,7 +30,7 @@ public class SpriteHandle extends ResourceHandle {
 
 	@Override
 	public void load(Core core) throws ResourceException {
-		Texture texture = (Texture) ResourceService.getInstance(core).getResource(this.textureId);
+		Texture texture = (Texture) ResourceService.getInstance(core).getResource(this.textureName);
 		
 		if (this.width == 0 || this.height == 0) {
 			this.sprite = new SpriteVisual(texture, texture.getImageWidth(), texture.getImageHeight());
