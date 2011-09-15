f762722539fcdde9e4ccffe67be58f8c12cd11c0
Roman Divotkey <roman@divotkey.net>
2011-09-15 13:31:22 +0200

    Added interface ReadableColor.

---
+++ Cogaen LWJGL/src/org/cogaen/lwjgl/scene/ReadableColor.java
@@ -0,0 +1,10 @@
+package org.cogaen.lwjgl.scene;
+
+public interface ReadableColor {
+	
+	public double getRed();
+	public double getGreen();
+	public double getBlue();
+	public double getAlpha();
+
+}
