b30ee18341771dc436158e0ae0742425a2468f94
Roman Divotkey <roman@divotkey.net>
2011-10-23 13:22:38 +0200

    New abstract base class for HUD classes.

---
+++ Cogaen Core/src/org/cogaen/view/AbstractHud.java
@@ -0,0 +1,33 @@
+package org.cogaen.view;
+
+import org.cogaen.core.Core;
+import org.cogaen.core.Engageable;
+
+public class AbstractHud implements Engageable {
+
+	private Core core;
+	private boolean engaged;
+	
+	public AbstractHud(Core core) {
+		this.core = core;
+	}
+	
+	@Override
+	public void engage() {
+		this.engaged = true;
+	}
+
+	@Override
+	public void disengage() {
+		this.engaged = false;
+	}
+
+	@Override
+	public final boolean isEngaged() {
+		return this.engaged;
+	}
+
+	public final Core getCore() {
+		return this.core;
+	}
+}
