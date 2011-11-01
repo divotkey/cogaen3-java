7441387443092caf5f590c383ef3b4547b667925
Roman Divotkey <roman@divotkey.net>
2011-11-01 13:15:19 +0100

    Added BlinkTask.

---
+++ Cogaen LWJGL/src/org/cogaen/lwjgl/scene/BlinkTask.java
@@ -0,0 +1,38 @@
+package org.cogaen.lwjgl.scene;
+
+import org.cogaen.core.Core;
+import org.cogaen.task.AbstractTask;
+import org.cogaen.time.TimeService;
+import org.cogaen.time.Timer;
+
+public class BlinkTask extends AbstractTask {
+
+	private double interval;
+	private Timer timer;
+	private Visual visual;
+	private int mask;
+	private double timeStamp;
+	
+	public BlinkTask(Core core, Visual visual, double interval) {
+		super(core, "Blink");
+		this.interval = interval;
+		this.timer = TimeService.getInstance(getCore()).getTimer();
+		this.visual = visual;
+		this.mask = this.visual.getMask();
+		this.timeStamp = this.timer.getTime() + this.interval;
+	}
+
+	@Override
+	public void update() {
+		if (this.timeStamp <= this.timer.getTime()) {
+			this.visual.setMask(this.visual.getMask() == this.mask ? 0x000 : this.mask);
+			this.timeStamp = this.timer.getTime() + this.interval;
+		}
+	}
+
+	@Override
+	public void destroy() {
+		// intentionally left empty
+	}
+
+}
