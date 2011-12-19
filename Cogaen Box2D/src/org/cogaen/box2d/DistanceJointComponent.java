4d41f932b73b9b23944e72b9fa8830702d628d77
Roman Divotkey <roman@divotkey.net>
2011-12-19 12:54:58 +0100

    Added method setCollideConnected to DistanceJointComponent.

--- Cogaen Box2D/src/org/cogaen/box2d/DistanceJointComponent.java
+++ Cogaen Box2D/src/org/cogaen/box2d/DistanceJointComponent.java
@@ -15,6 +15,7 @@ public class DistanceJointComponent extends Component {
 	private double anchorBx;
 	private double anchorBy;
 	private ComponentEntity entityB;
+	private boolean collide = false;
 	
 	public DistanceJointComponent(double anchorAx, double anchorAy,	double anchorBx, double anchorBy, ComponentEntity entityB) {
 		this.anchorAx = anchorAx;
@@ -54,16 +55,20 @@ public class DistanceJointComponent extends Component {
 		Body bodyB = getBody(this.entityB);
 		Vec2 a1 = bodyA.getWorldPoint(new Vec2((float) anchorAx, (float) anchorAy));
 		Vec2 a2 = bodyB.getWorldPoint(new Vec2((float) anchorBx, (float) anchorBy));
-		djd.collideConnected = true;
+		djd.collideConnected = this.collide;
 		djd.initialize(bodyA, bodyB, a1, a2);
-		djd.dampingRatio = 1f;
-		djd.frequencyHz = 0;
+//		djd.dampingRatio = 1.0f;
+//		djd.frequencyHz = 30;
 		PhysicsService.getInstance(getCore()).getWorld().createJoint(djd);
 	}
 		
 	private Body getBody(ComponentEntity entity) {
 		Box2dBody boxBody = (Box2dBody) entity.getAttribute(Box2dBody.BOX2D_BODY_ATTRIB);
 		return boxBody.getBody();
+	}
+	
+	public void setCollideConnected(boolean value) {
+		this.collide = value;
 	}
 	
 	
